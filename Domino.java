1b4d3694eb671b40b39b84ce15e4803e5a65c119
emma2105 <45943651+emma2105@users.noreply.github.com>
2018-12-17 16:15:07 +0100

    Add files via upload

--- Domino.java
+++ Domino.java
@@ -1,86 +1,83 @@
-	
-	public class Domino 
-	
-	{
-		int faceNumero;
-		String facePaysageDroite;
-		String facePaysageGauche;
-		/**boolean nombreCouronne1 = false;  booléen car la réponse est soit oui soit non donc soit 1 soit 0
-		boolean nombreCouronne2 = false; **/
-		int nombreCouronne1;
-		int nombreCouronne2;
-	
-	
-		String orientation;
-		
-		/* Création du constructeur*/
-		Domino(int faceNumero,	String type1, String type2,String orientation, int couronne1, int couronne2)
-		{
-			this.faceNumero = faceNumero;
-			this.facePaysageDroite = type1;
-			this.facePaysageGauche = type2;
-			this.orientation = orientation;
-			this.nombreCouronne1 = couronne1;
-			this.nombreCouronne2 = couronne2;
-	
-			
-			
-		}
-		
-		// retourne la face numéro du domino
-		public int getFaceNumero() 
-		{
-			return faceNumero;
-		}
-		
-		// retourne le côté droit de la face paysage du domino
-		public String getfacePaysageDroite() 
-		{
-			return facePaysageDroite;
-		}
-	
-		// retourne le côté gauche de la face paysage du domino
-		public String getfacePaysageGauche() 
-		{
-			return facePaysageGauche;
-		}
-		
-		// retourne l'orientation du domino
-		public String getOrientation() 
-		{
-			return orientation;
-		}
-		
-		// retourne le nombre de couronne1
-		public int getCouronne1() 
-		{
-			return nombreCouronne1;
-		}
-		
-		// retourne le nombre de couronne2
-				public int getCouronne2() 
-				{
-					return nombreCouronne2;
-				}
-		// Définit le nombre de couronne1
-		void setCouronne1(int nombre) 
-		{
-			 nombreCouronne1 += nombre;
-	
-		}
-		
-		// Définit le nombre de couronne2
-	     void setCouronne2(int nombre) 
-		{
-			 nombreCouronne2 += nombre;
 
+public class Domino {
 	
-	    }
+	//Initialisation
+	private int face_numero;
+	private String face_paysageDroit;
+	private String face_paysageGauche;
+	private int nbCouronneDroit;
+	private int nbCouronneGauche;
+	private int orientation = 0;
+	
+	
+	// Constructeur
+	Domino(int face_numero, String face_paysageDroit, String face_paysageGauche, int nbCouronneDroit, int nbCouronneGauche, int orientation){
 		
+		this.face_numero = face_numero;
+		this.face_paysageDroit = face_paysageDroit;
+		this.face_paysageGauche = face_paysageGauche;
+		this.nbCouronneDroit = nbCouronneDroit;
+		this.nbCouronneGauche = nbCouronneGauche;
+		this.orientation = orientation;
+	}
 	
-	 
 	
-
-
+	// Face numero 
+	public void setFace_numero(int numero) {
+		face_numero = numero;
+	}
+	
+	public int getFace_numero() {
+		return face_numero;
+	}
+	
+	// Face paysage droit
+	public void setFace_paysageDroit(String paysage) {
+		face_paysageDroit=paysage;
+		
+	}
+	public String getFace_paysageDroit() {
+		return face_paysageDroit;
+	}
+	
+	// Face paysage Gauche
+	public void set_face_paysageGauche(String paysage) {
+		face_paysageGauche=paysage;
+		
+	}
+	
+	public String getFace_paysageGauche() {
+		return face_paysageGauche;
+	}
+	
+	
+	// nombre de couronne cote droit
+	public void setNbCouronneDroit(int nouveauNbCouronneDroit) {
+		nbCouronneDroit = nouveauNbCouronneDroit;
+	}
+	public int getNbCouronneDroit() {
+		return nbCouronneDroit;
+	}
+	
+	
+	// nombre couronne cote gauche
+	public void setNbCouronneGauche(int nouveauNbCouronneGauche) {
+		nbCouronneGauche = nouveauNbCouronneGauche;
+	}
+	
+	public int getNbCouronneGauche() {
+		return nbCouronneGauche;
+	}
+	
+	public void setOrientation(int nouveauOrientation) {
+		orientation = nouveauOrientation;
+		
+	}
+	
+	public int getOrientation() {
+		return orientation;
+	}
+	
+	
 
 }
